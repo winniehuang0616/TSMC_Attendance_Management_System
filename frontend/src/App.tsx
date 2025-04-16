6b215a9f7dcc7625f740c384ab82fe87ab848555
winnie0616 <winniebear1213@gmail.com>
2025-04-17 05:39:56 +0800

    style:format

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
 function App() {
   return (
     <>
-      <div className="flex flex-col h-screen">
-        <div className="h-[14%] bg-white shadow-header relative z-10"></div> 
+      <div className="flex h-screen flex-col">
+        <div className="relative z-10 h-[14%] bg-white shadow-header"></div>
         <div className="flex flex-1">
           <div className="w-[18%] bg-white shadow-sidebar"></div>
-          <div className="flex-1 bg-background flex items-center justify-center">
-          </div>
+          <div className="flex flex-1 items-center justify-center bg-background"></div>
         </div>
       </div>
     </>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,11 +6,11 @@ export default {
     extend: {
       colors: {
         background: "#EDF1F7",
-        blue: '#4154F1',
-        darkBlue: '#012970',
-        gray: '#6C757D',
-        pink: '#FF4170',
-        purple: '#F6F6FE',
+        blue: "#4154F1",
+        darkBlue: "#012970",
+        gray: "#6C757D",
+        pink: "#FF4170",
+        purple: "#F6F6FE",
       },
       borderRadius: {
         lg: "var(--radius)",
@@ -18,10 +18,10 @@ export default {
         sm: "calc(var(--radius) - 4px)",
       },
       boxShadow: {
-        'header': '0px 4px 20px 2px rgba(0, 0, 0, 0.03)',
-        'sidebar': '0px 0px 20px 0px rgba(0, 0, 0, 0.03)',
-        'element': '0px 0px 30px 0px rgba(1, 41, 112, 0.1)',
-      }
+        header: "0px 4px 20px 2px rgba(0, 0, 0, 0.03)",
+        sidebar: "0px 0px 20px 0px rgba(0, 0, 0, 0.03)",
+        element: "0px 0px 30px 0px rgba(1, 41, 112, 0.1)",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
