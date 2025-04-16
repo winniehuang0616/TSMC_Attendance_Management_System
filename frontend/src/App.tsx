fd3bced061c41bd4caa137565802af5fc439d4e3
winnie0616 <winniebear1213@gmail.com>
2025-04-17 05:38:22 +0800

    feat:initial layout & tailwind config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
-import { DemoCard } from "@/components/demoCard";
-
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <DemoCard />
-    </div>
+    <>
+      <div className="flex flex-col h-screen">
+        <div className="h-[14%] bg-white shadow-header relative z-10"></div> 
+        <div className="flex flex-1">
+          <div className="w-[18%] bg-white shadow-sidebar"></div>
+          <div className="flex-1 bg-background flex items-center justify-center">
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,13 +5,23 @@ export default {
   theme: {
     extend: {
       colors: {
-        blue: "#90caf9",
+        background: "#EDF1F7",
+        blue: '#4154F1',
+        darkBlue: '#012970',
+        gray: '#6C757D',
+        pink: '#FF4170',
+        purple: '#F6F6FE',
       },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      boxShadow: {
+        'header': '0px 4px 20px 2px rgba(0, 0, 0, 0.03)',
+        'sidebar': '0px 0px 20px 0px rgba(0, 0, 0, 0.03)',
+        'element': '0px 0px 30px 0px rgba(1, 41, 112, 0.1)',
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
