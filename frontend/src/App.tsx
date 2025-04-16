525205df1a0cfaf5b2bb4bd55fed8691a43e8c80
winnie0616 <winniebear1213@gmail.com>
2025-04-17 03:13:18 +0800

    env:initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { DemoCard } from "@/components/demoCard";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <DemoCard />      
+    </div>
+  );
+}
+
+export default App;
