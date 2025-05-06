b4bc94d3a88e8b834e74be70dbf93ad7d3559274
jiasin <w6522873@gmail.com>
2025-05-06 14:39:21 +0800

    test

---
+++ backend/tests/test_department_leaves.py
@@ -0,0 +1,23 @@
+# test_department_leaves.py
+from fastapi.testclient import TestClient
+from src.main import app
+
+client = TestClient(app)
+
+def test_valid_manager():
+    response = client.get("/leaves/manager/EMP001/department-leaves")
+    assert response.status_code == 200
+
+# def test_empty_department():
+#     response = client.get("/leaves/manager/M54321/department-leaves")
+#     assert response.status_code == 200
+#     assert response.json() == []
+
+def test_invalid_manager_id():
+    response = client.get("/leaves/manager/INVALID_ID/department-leaves")
+    assert response.status_code < 500
+
+def test_non_manager_id():
+    response = client.get("/leaves/manager/EMP002/department-leaves")
+    assert response.status_code < 500
+
