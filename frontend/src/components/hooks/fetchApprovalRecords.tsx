1836284fcd6c5f3eb9777d16915030d6b9f6431e
meng chin <apple912162@gmail.com>
2025-05-13 14:31:36 +0800

    lint

--- frontend/src/components/hooks/fetchApprovalRecords.tsx
+++ frontend/src/components/hooks/fetchApprovalRecords.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 import axios from "axios";
+
 import type { RawLeaveRecord } from "./fetchLeaveRecord";
 
 export function useApprovalRecords(userId: string | null) {
